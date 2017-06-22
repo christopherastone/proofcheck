4e76be5a710119fc7f6b195fc45884b103d5ff7b
Christopher A. Stone <stone@cs.hmc.edu>
2017-06-22 15:59:14 -0700

    Added comments

--- ccg/parser.py
+++ ccg/parser.py
@@ -4,40 +4,36 @@
 """
 Created on Tue May 30 14:13:01 2017
 
-@author: stone
+@author: Chris Stone
 """
 
 import re
 
-import category
+from category import NP, S, VBI, VBT, MODAL
+from item import Item
 import semantics as sem
 import rules
-from item import Item
-
-NP = category.NP
-S = category.S
-VBI = category.VBI
-VBT = category.VBT
-MODAL = category.MODAL
 
 
-def words(s: str):
-    """Strips the punctuation and just returns the words"""
-    return re.sub(r'[^A-za-z]', ' ', s).lower().split()
-
+# LEXICON CONSTRUCTION
 
 def pn(wd):
-    """Creates a NP entry for the lexicon, with the given name"""
+    """Creates a NP entry for the lexicon, with the given proper name
+       and the semantics of a constant
+    """
     return Item(NP, sem.Const(wd), wd)
 
 
 def intrans(vb):
-    """Creates an intransitive-verb entry the lexicon, with the given name"""
+    """Creates an intransitive-verb entry the lexicon, with the given name
+       and the semantics of a constant (or equivalently, λx. vb x )
+    """
     return Item(VBI, sem.Const(vb), vb)
 
 
 def trans(vb):
-    """Creates a transitive-verb entry the lexicon, with the given name"""
+    """Creates a transitive-verb entry the lexicon, with the given name
+       adnd the semantics λy. λx. vb x  y"""
     return Item(VBT,
                 sem.Lam(sem.gensym("y"),
                         sem.Lam(sem.gensym("x"),
@@ -48,6 +44,8 @@ def trans(vb):
 
 
 def modal(wd):
+    """Creates a modal-verb entry in the lexicon, with the given name
+       and the semantics λf. λx. wd(f x) """
     return Item(MODAL,
                 sem.Lam(sem.gensym("f"),
                         sem.Lam(sem.gensym("x"),
@@ -55,6 +53,11 @@ def modal(wd):
                                         sem.App(sem.BoundVar(1),
                                                 sem.BoundVar(0))))),
                 wd)
+
+
+################
+# TEST LEXICON #
+################
 
 
 LEXICON = {'fido': [pn('fido')],
@@ -70,9 +73,15 @@ LEXICON = {'fido': [pn('fido')],
            'will': [modal('will')]}   # baldridge p. 24-25
 
 
+###########
+# PARSING #
+###########
+
+
 def mkChart(wds):
-    """Creates an initial chart for the given words, with just the
-    lexicon information for each word/leaf"""
+    """Creates an initial chart for the given list of words.
+       It starts out near-empty, with just the
+       lexicon information for each word/leaf."""
     chart = {}
     for i in range(len(wds)):
         # We clone the lexicon list because unary promotion rules can
@@ -83,20 +92,28 @@ def mkChart(wds):
 
 
 def fillCell(chart, i, j, rules=rules.parsingRules):
+    """Update the chart to fill in cell (i,j), which covers
+       the i-th word to the j-th word, INCLUSIVE. It works by
+       applying all the binary rules to all possible ways to
+       partition the range into two nonempty sub-segments,
+       and then applies all the unary rules to the results."""
     if (i, j) not in chart:
         chart[(i, j)] = []
     # print(f'chart[({i},{j})] = {[str(i) for i in chart[(i,j)]]} (1)')
     for d in range(j-i):
         # print(f'combining ({i},{i+d}) with ({i+d+1},{j})')
-        cell1 = chart[(i, i + d)]
-        cell2 = chart[(i + d + 1, j)]
+        cell1 = chart[(i, i + d)]      # parse of the first d words of segment
+        cell2 = chart[(i + d + 1, j)]  # parse of the remaining words
         # print(f'cell1 = {cell1} and cell2 = {cell2}')
         for item1 in cell1:
             for item2 in cell2:
                 # print(f'checking ({cat1},{sem1}) & ({cat2},{sem2})')
+                # Apply all the binary rules to the partial parses
                 for binaryRule in rules[1]:
                     binaryRule(item1, item2, chart[(i, j)])
     # print(f'chart[({i},{j})] = {[str(i) for i in chart[(i,j)]]} (2)')
+    # Iterate over a copy of the cell that we're adding to, so we don't
+    # apply unary rules to unary-rule-results. [Is this ever a problem?]
     for item in chart[(i, j)][:]:
         for unaryRule in rules[0]:
             # print(f'considering {item} for unary rule {unaryRule}')
@@ -104,7 +121,14 @@ def fillCell(chart, i, j, rules=rules.parsingRules):
     # print(f'chart[({i},{j})] = {[str(i) for i in chart[(i,j)]]} (3)')
 
 
+def words(s: str):
+    """Strip ALL punctuation and just returns the (lowercased) words
+       as a list"""
+    return re.sub(r'[^A-za-z]', ' ', s).lower().split()
+
+
 def parse(sentence):
+    """parse the given string and return all complete parses"""
     wds = words(sentence)
     nwds = len(wds)
     chart = mkChart(wds)
@@ -118,6 +142,7 @@ def parse(sentence):
 
 
 def p(sentence):
+    """parse the given string, and pretty-print all complete parses"""
     items = parse(sentence)
     for item in items:
         print()
