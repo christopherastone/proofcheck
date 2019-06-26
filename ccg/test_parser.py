62c61358ddc8c934824afdff8df7502ba84c9e40
Christopher A. Stone <stone@cs.hmc.edu>
2019-06-26 11:10:57 -0700

    command-line input for test_parser.py

--- ccg/test_parser.py
+++ ccg/test_parser.py
@@ -9,6 +9,7 @@ Created on Tue May 30 14:13:01 2017
 import category
 import chartparser
 import catparser
+import sys
 
 
 def test_words():
@@ -27,12 +28,16 @@ def test_parse():
     assert str(ans[0].sem) == "λx.will(eat(x)(cheese))"
 
 
-def test_lexicon():
-    lexicon_data = open('lexicon.txt').read().splitlines()
+def test_lexicon(filename):
+    lexicon_data = open(filename).read().splitlines()
     lexicon, sentences = catparser.do_parses(lexicon_data)
     for (label, sentence, category, expected_count) in sentences:
         chartparser.p(label, sentence, lexicon, category, expected_count)
 
 
 if __name__ == '__main__':
-    test_lexicon()
+    if len(sys.argv) > 1:
+        for filename in sys.argv[1:]:
+            test_lexicon(filename)
+    else:
+        test_lexicon('lexicon.txt')
